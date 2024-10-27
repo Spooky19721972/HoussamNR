b6703103596ffdcc5d9fcde5d44ded9dc34bfde6
HOUSSAM Niri <100099640+Spooky19721972@users.noreply.github.com>
2024-10-27 14:56:26 +0100

    Update js_profile.js

--- js_profile.js
+++ js_profile.js
@@ -1,17 +1,13 @@
-// Get the form element by its ID
-const form = document.getElementById('contactForm');
 
-// Add an event listener to the form to handle form submission
+const form = document.getElementById('contactForm');
 form.addEventListener('submit', function(event) {
-    // Prevent the default form submission
     event.preventDefault();
 
-    // Get form field values
+    
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const message = document.getElementById('textarea').value.trim();
 
-    // Perform validation
     if (name === '') {
         alert('Please enter your name.');
         return;
@@ -27,11 +23,9 @@ form.addEventListener('submit', function(event) {
         return;
     }
 
-    // If all validations pass, submit the form (you can add an actual submit logic here)
     alert('Form submitted successfully!');
 });
 
-// Function to validate email format using a regular expression
 function validateEmail(email) {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return emailPattern.test(email);
